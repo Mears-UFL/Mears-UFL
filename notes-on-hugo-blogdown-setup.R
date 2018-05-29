424b881351e8d44a4d2f34eb9f88999fc8b45637
Mears-UFL <mears@ufl.edu>
2018-05-29 12:16:32 -0400

    Combined new project blogdown-hugo-academic

---
+++ notes-on-hugo-blogdown-setup.R
@@ -0,0 +1,8 @@
+library(devtools)
+library(blogdown)
+
+#blogdown::new_site(theme = 'user/repo') # old way to install
+
+# blogdown::install_hugo() #new way to install (then restart rstudio)
+# https://sourcethemes.com/academic/docs/install/#install-with-rstudio
+
